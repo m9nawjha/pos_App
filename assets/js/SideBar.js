1ad2d413bb40fd5e4eaa93c69dd80ca3a9f10880
m9nawjha <m.nawjha@gmail.com>
2023-10-01 04:42:36 +0300

    App pos

--- assets/js/SideBar.js
+++ assets/js/SideBar.js
@@ -1,13 +1,203 @@
 const navigationMenu =[
+
     {
         text: 'الرئيسية',
-        href: 'index.html',
+        href: 'hamePage/index.html',
         icon: 'bi bi-house-fill',
-        children: null,
+        children: false,
+    },
+    
+    {
+        text: 'نقاط البيع',
+        href: 'pages/pos/index.html',
+        icon: 'bi bi-ui-checks-grid',
+        children: false,
+    },
+    {
+        text: 'المنتجات',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-file-earmark-ppt-fill',
+        children: [
+            {
+                text: 'قائمة المنتجات',
+                href: 'pages/products/list.html',
+            },
+            {
+                text: 'إضافة منتج',
+                href: 'pages/products/add.html',
+            },
+            {
+                text: 'إستيراد منتجات',
+                href: 'pages/products/import.html',
+            },
+            {
+                text: 'طباعة باركود',
+                href: 'pages/products/barcode.html',
+            },
+            {
+                text: 'طباعة ملصق',
+                href: 'pages/products/stuck.html',
+            },
+        ],
+    },
+    {
+        text: 'الفئات',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-folder-fill',
+        children: [
+            {
+                text: 'قائمة الفئات',
+                href: 'pages/categories/list.html',
+            },
+            {
+                text: 'إضافة فئة',
+                href: 'pages/categories/add.html',
+            },
+            {
+                text: 'إستيراد فئات',
+                href: 'pages/categories/import.html',
+            },
+        ],
+    },
+    {
+        text: 'المبيعات',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-cart-plus-fill',
+        children: [
+            {
+                text: 'قائمة المبيعات',
+                href: 'pages/sales/list.html',
+            },
+            {
+                text: 'الفواتير المفتوحة',
+                href: 'pages/sales/opened.html',
+            },
+        ],
+    },
+    {
+        text: 'المشتريات',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-bag-check-fill',
+        children: [
+            {
+                text: 'قائمة المشتريات',
+                href: 'pages/purchases/list.html',
+            },
+            {
+                text: 'إضافة مشتريات',
+                href: 'pages/purchases/add.html',
+            },
+            {
+                text: 'قائمة المصروفات',
+                href: 'pages/purchases/expenses.html',
+            },
+            {
+                text: 'إضافة مصروفات',
+                href: 'pages/purchases/addExpense.html',
+            },
+        ],
+    },
+    {
+        text: 'بطاقات الهدايا',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-gift-fill',
+        children: [
+            {
+                text: 'قائمة البطاقات',
+                href: 'pages/giftCards/list.html',
+            },
+            {
+                text: 'إضافة بطاقة',
+                href: 'pages/giftCards/add.html',
+            },
+        ],
+    },
+    {
+        text: 'الموظفين',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-person-vcard-fill',
+        children: [
+            {
+                text: 'قائمة الموظفين',
+                href: 'pages/users/list.html',
+            },
+            {
+                text: 'إضافة موظف',
+                href: 'pages/users/add.html',
+            },
+        ],
+    },
+    {
+        text: 'العملاء',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-people-fill',
+        children: [
+            {
+                text: 'قائمة العملاء',
+                href: 'pages/customers/list.html',
+            },
+            {
+                text: 'إضافة عميل',
+                href: 'pages/customers/add.html',
+            },
+        ],
+    },
+    {
+        text: 'الموردين',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-person-fill-up',
+        children: [
+            {
+                text: 'قائمة الموردين',
+                href: 'pages/suppliers/list.html',
+            },
+            {
+                text: 'إضافة مورد',
+                href: 'pages/suppliers/add.html',
+            },
+        ],
+    },
+    {
+        text: 'المتاجر',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-shop',
+        children: [
+            {
+                text: 'عرض المتاجر',
+                href: 'pages/stores/all.html',
+            },
+            {
+                text: 'إضافة متجر',
+                href: 'pages/stores/add.html',
+            },
+        ],
+    },
+    {
+        text: 'الإعدادت',
+        href: 'javascript:void(0)',
+        icon: 'bi bi-gear-fill',
+        children: [
+            {
+                text: 'إعدادات النظام',
+                href: 'pages/settings/index.html',
+            },
+            {
+                text: 'الطابعات',
+                href: 'pages/settings/printers.html',
+            },
+            {
+                text: 'إضافة طابعة',
+                href: 'pages/settings/add-printer.html',
+            },
+            {
+                text: 'النسخ الاحتياطي',
+                href: 'pages/settings/backups.html',
+            },
+        ],
     },
     {
         text: 'التقارير',
-        href: 'javascript:',
+        href: 'javascript:void(0)',
         icon: 'bi bi-pie-chart-fill',
         children: [
             {
@@ -20,9 +210,31 @@ const navigationMenu =[
             },
             {
                 text: 'تقرير المبيعات',
+                href: 'pages/reports/sales.html',
+            },            
+            {
+                text: 'تقرير المدفوعات',
                 href: 'pages/reports/payments.html',
             },
+            {
+                text: 'تقارير السجلات',
+                href: 'pages/reports/registers.html',
+            },
+            {
+                text: 'أفضل المنتجات',
+                href: 'pages/reports/top_products.html',
+            },
+            {
+                text: 'تقارير المنتجات',
+                href: 'pages/reports/products.html',
+            },
         ],
+    },
+    {
+        text: 'تسجيل الخروج',
+        href: 'logout.html',
+        icon: 'bi bi-box-arrow-left',
+        children: false,
     },
 ]
 function menuFunction(){
@@ -34,9 +246,9 @@ function menuFunction(){
                 <a class='side-menu__item' data-bs-toggle='slide' href='${sideItem.href}'>
                 <i class='side-menu__icon ${sideItem.icon}'></i>
                 <span class='side-menu__label'>${sideItem.text}</span>
-                ${sideItem.children===null ? '' : `<i class='angle fs-6 fw-bold bi bi-chevron-left'></i>`}
+                ${sideItem.children===false ? '' : `<i class='angle fs-6 fw-bold bi bi-chevron-left'></i>`}
                 </a>
-                ${sideItem.children===null ? '' : `<ul class='slide-menu'>
+                ${sideItem.children===false ? '' : `<ul class='slide-menu'>
                 <li class='side-menu-label1'><a href='javascript:void(0)'>${sideItem.text}</a></li>
                 ${sideItem.children.map((childItem)=> `<li><a href='${childItem.href}' class='slide-item'>${childItem.text}</a></li>`).join('')}
                 </ul>
--- assets/js/appMain.js
+++ assets/js/appMain.js
@@ -1,25 +1,61 @@
-// const importJS =(dir)=>{
-//     let scriptElement = document.createElement('script');
-//     scriptElement.src = dir;
-//     document.body.appendChild(scriptElement);
-// }
-// const importCSS =(dir)=>{
-//     let styleElement = document.createElement('link');
-//     styleElement.href = dir;
-//     styleElement.rel = 'stylesheet';
-//     document.head.appendChild(styleElement);
-// }
+
+const importJS =(dir)=>{
+    let scriptElement = document.createElement('script');
+    scriptElement.src = dir;
+    document.body.appendChild(scriptElement);
+}
+const importCSS =(dir)=>{
+    let styleElement = document.createElement('link');
+    styleElement.href = dir;
+    styleElement.rel = 'stylesheet';
+    document.head.appendChild(styleElement);
+}
 
 // import JS files
-// importJS('assets/library/jquery/jquery.min.js');
-// importJS('assets/library/popper/popper.min.js');
-// importJS('assets/library/bootstrap/bootstrap.min.js');
-// importJS('assets/library/chart/chart.js');
-// importJS('assets/library/p-scroll/perfect-scrollbar.js');
-// importJS('assets/library/p-scroll/pscroll.js');
-// importJS('assets/library/p-scroll/pscroll-1.js');
-// importJS('assets/library/sidemenu/sidemenu.js');
-// importJS('assets/js/index-chartclear.js');
-// importJS('assets/js/themeColors.js');
-console.log('scend');
-// import styles CSS 
+
+importJS('assets/js/jquery.min.js');
+importJS('assets/library/bootstrap/js/popper.min.js');
+importJS('assets/library/bootstrap/js/bootstrap.min.js');
+importJS('assets/library/charts-c3/d3.v5.min.js');
+importJS('assets/library/charts-c3/c3-chart.js');
+importJS('assets/library/input-mask/jquery.mask.min.js');
+importJS('assets/js/SideBar.js');
+importJS('assets/js/tooltip&popover.js');
+importJS('assets/library/sidemenu/sidemenu.js');
+importJS('assets/library/bootstrap5-typehead/autocomplete.js');
+importJS('assets/js/typehead.js');
+importJS('assets/js/charts.js');
+importJS('assets/js/app-chart.js');
+importJS('assets/library/select2/select2.full.min.js');
+importJS('assets/library/datatable/js/jquery.dataTables.min.js');
+importJS('assets/library/datatable/js/dataTables.bootstrap5.js');
+importJS('assets/library/datatable/js/dataTables.buttons.min.js');
+importJS('assets/library/datatable/js/buttons.bootstrap5.min.js');
+importJS('assets/library/datatable/js/jszip.min.js');
+importJS('assets/library/datatable/pdfmake/pdfmake.min.js');
+importJS('assets/library/datatable/pdfmake/vfs_fonts.js');
+importJS('assets/library/datatable/js/buttons.html5.min.js');
+importJS('assets/library/datatable/js/buttons.print.min.js');
+importJS('assets/library/datatable/js/buttons.colVis.min.js');
+importJS('assets/library/datatable/dataTables.responsive.min.js');
+importJS('assets/library/datatable/responsive.bootstrap5.min.js');
+importJS('assets/js/table-data.js');
+importJS('assets/js/autofill-table.js');
+importJS('assets/library/sidebar/sidebar.js');
+importJS('assets/library/p-scroll/perfect-scrollbar.js');
+importJS('assets/library/p-scroll/pscroll.js');
+importJS('assets/library/p-scroll/pscroll-1.js');
+importJS('assets/library/wysiwyag/jquery.richtext.js');
+importJS('assets/library/wysiwyag/wysiwyag.js');
+importJS('assets/js/themeColors.js');
+importJS('assets/js/sticky.js');
+importJS('assets/js/custom.js');
+
+
+// import style css
+
+
+// date & time
+const timeDate ='<i class="bi bi-stopwatch fs-6 mx-1 text-blue"></i>' + new Date().toLocaleString('ar-SA' , {dateStyle: 'short' , timeStyle:'short'});
+document.getElementById('date_time').innerHTML = timeDate;
+
